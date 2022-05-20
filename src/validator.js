fdee6ce87902051d4499421a0cd15a202f28571b
jannerymbf <jannerymai@gmail.com>
2022-05-20 10:33:27 -0500

    Implem

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,39 @@
 const validator = {
-  // ...
+  cardNumber : document.getElementById("creditCardNumber").value,
+  isValid: function(){
+    let arrayCardNumber=Array.from(this.cardNumber);
+    let arrayCardNumber2=arrayCardNumber.reverse();
+
+  let finalArray=arrayCardNumber2.map(function(element, position) {
+    parseInt(element);
+    if(position%2!==0){
+      let newElement=element*2;
+      if(newElement>9){
+        let numberToString=newElement.toString();
+        let digit1=parseInt(numberToString.cnharAt(numberToString.length-1));
+        let digit2=parseInt(numberToString.charAt(numberToString.length-2));
+        let suma=digit1+digit2;
+        return suma;
+      }else{
+        return newElement;
+      }
+    }else{
+      return parseInt(element);
+    }
+
+  })
+
+  let totalNumber=finalArray.reduce((acumulador, valorActual)=>
+  acumulador+valorActual,0);
+
+  if (totalNumber%10===0){
+    alert("Numero Valido");
+  }else{
+    alert("Numero Invalido");
+  }
+
+  }
 };
+
 
 export default validator;
