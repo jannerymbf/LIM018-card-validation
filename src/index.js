ce7a8f8b7cdd53e024023d5e445b269f6f883709
jannerymbf <jannerymai@gmail.com>
2022-05-26 11:54:20 -0500

    Validacion y maskify listos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import validator from './validator.js'; //dejarlo en la primera línea
 
 let creditCardNumber=document.getElementById("creditCardNumber");
+let name=document.getElementById("name");
+let month=document.getElementById("month");
+let year=document.getElementById("year");
+let CVV=document.getElementById("CVV");
 let validar=document.getElementById("validar");
+let limpiar=document.getElementById("clean");
 
 validar.addEventListener("click", ()=>{
     let finalMessage=document.getElementById("message");
+    
+
     if(creditCardNumber.value===""){
         finalMessage.innerHTML="*Ingrese un número de tarjeta.";
     }else{
@@ -14,8 +21,21 @@ validar.addEventListener("click", ()=>{
             finalMessage.innerHTML="*El número de tarjeta ingresado es inválido.";
         }
     }
+
+    creditCardNumber.value=validator.maskify(creditCardNumber.value);
     
+
 });
+
+limpiar.addEventListener("click", ()=>{
+    creditCardNumber.value="";
+    name.value="";
+    month.value="";
+    year.value="";
+    CVV.value="";
+});
+
+
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -2,33 +2,53 @@ const validator = {
   isValid: function(cardNumber){
     let arrayCardNumber=Array.from(cardNumber);
     let arrayCardNumber2=arrayCardNumber.reverse();
-
-  let finalArray=arrayCardNumber2.map(function(element, position) {
-    parseInt(element);
-    if(position%2!==0){
-      let newElement=element*2;
-      if(newElement>9){
-        let numberToString=newElement.toString();
-        let digit1=parseInt(numberToString.charAt(numberToString.length-1));
-        let digit2=parseInt(numberToString.charAt(numberToString.length-2));
-        let suma=digit1+digit2;
-        return suma;
-      }else{
+    
+    let finalArray=arrayCardNumber2.map(function(element, position) {
+      parseInt(element);
+      if(position%2!==0){
+        let newElement=element*2;
+        if(newElement>9){
+          let numberToString=newElement.toString();
+          let digit1=parseInt(numberToString.charAt(numberToString.length-1));
+          let digit2=parseInt(numberToString.charAt(numberToString.length-2));
+          let suma=digit1+digit2;
+          return suma;
+        }else{
         return newElement;
+        }
+      }else{
+        return parseInt(element);
       }
-    }else{
-      return parseInt(element);
+
+    })
+
+    let totalNumber=finalArray.reduce((acumulador, valorActual)=>
+    acumulador+valorActual,0);
+
+    return (totalNumber%10===0)?true:false;
+  },
+
+  maskify: function(cardNumber){
+    let arrayCard=Array.from(cardNumber);
+    let newStringCardNumber;
+    let newCardNumber;
+
+    if(cardNumber===""){
+      return "";
+    } else if(cardNumber.length===1 || cardNumber.length===2 ||cardNumber.length===3 || cardNumber.length===4){
+      return cardNumber;
+    } else if(cardNumber.length>=5){
+      for(let i=0; i<cardNumber.length-4;i++){
+        arrayCard[i]=arrayCard[i].replace(arrayCard[i],"#");
+        newStringCardNumber=arrayCard.toString();
+      }
     }
 
-  })
-
-  let totalNumber=finalArray.reduce((acumulador, valorActual)=>
-  acumulador+valorActual,0);
-
-  
-  return (totalNumber%10===0)?true:false;
-
+    newCardNumber=newStringCardNumber.replace(/,/g,"");
+    return newCardNumber;
   }
+
+
 };
 
 
