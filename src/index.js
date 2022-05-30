ef490d4503b39e77f7caa6b92d7d871fe6c93ee1
jannerymbf <jannerymai@gmail.com>
2022-05-30 15:55:40 -0500

    Versión con validación y maskify al 95%

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import validator from './validator.js'; //dejarlo en la primera línea
 
 let creditCardNumber=document.getElementById("creditCardNumber");
-let name=document.getElementById("name");
-let month=document.getElementById("month");
-let year=document.getElementById("year");
-let CVV=document.getElementById("CVV");
 let validar=document.getElementById("validar");
 let limpiar=document.getElementById("clean");
+let finalMessage=document.getElementById("message");
 
 validar.addEventListener("click", ()=>{
-    let finalMessage=document.getElementById("message");
     
-
     if(creditCardNumber.value===""){
-        finalMessage.innerHTML="*Ingrese un número de tarjeta.";
+        finalMessage.innerHTML="**Ingrese un número de tarjeta.";
+        finalMessage.classList.add("invalid");
     }else{
         if (validator.isValid(creditCardNumber.value)===true ){
-            finalMessage.innerHTML="*El número de tarjeta ingresado es válido.";
+            finalMessage.innerHTML="**El número de tarjeta ingresado es válido.";
+            finalMessage.classList.add("valid");
+            
         } else {
-            finalMessage.innerHTML="*El número de tarjeta ingresado es inválido.";
+            finalMessage.innerHTML="**El número de tarjeta ingresado es inválido.";
+            finalMessage.classList.add("invalid");
         }
     }
 
@@ -28,11 +27,7 @@ validar.addEventListener("click", ()=>{
 });
 
 limpiar.addEventListener("click", ()=>{
-    creditCardNumber.value="";
-    name.value="";
-    month.value="";
-    year.value="";
-    CVV.value="";
+    location.reload();
 });
 
 
